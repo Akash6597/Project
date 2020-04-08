983019d574226cc42a2f85d041ff85189a94fe56
Akash6597 <akashdabgar29@gmail.com>
2020-04-08 10:11:49 +0530

    Module 5 Ex 1

---
+++ Sql/module5/Exercise1.sql
@@ -0,0 +1,19 @@
+Use AdventureWorks;
+Go
+
+
+CREATE TABLE Sales.MediaOutlet(
+	MediaOutletId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	MediaOutletName nvarchar(40) NULL,
+	Contact nvarchar(50) NULL,
+	City nvarchar(50) NULL
+);
+
+CREATE TABLE Sales.PrintMediaPlacement(
+	PrintMediaPlacementId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	MediaOutletId int NULL,
+	PlacementDate datetime NULL,
+	PublicationDate datetime NULL,
+	PlacementCost decimal(18, 2) NULL,
+
+);
