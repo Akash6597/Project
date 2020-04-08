e271e89f713b2443a54ca74ddc91c7ecac63ed83
Akash6597 <akashdabgar29@gmail.com>
2020-04-08 10:52:12 +0530

    Create Exercise2.sql

---
+++ Sql/module5/Exercise2.sql
@@ -0,0 +1,14 @@
+Use AdventureWorks;
+Go
+
+ALTER TABLE Sales.MediaOutlet
+DROP CONSTRAINT [PK__MediaOut__4B132627343B1653]
+
+ALTER TABLE Sales.MediaOutlet ADD CONSTRAINT IX_MediaOutlet UNIQUE CLUSTERED (
+MediaOutletID
+); 
+ALTER TABLE Sales.PrintMediaPlacement
+DROP CONSTRAINT [PK__PrintMed__7AF65BA27315909C]
+
+CREATE CLUSTERED INDEX CIX_PRINTMEDIAPLACEMENT 
+ON Sales.PrintMediaPlacement (PrintMediaPlacementId ASC);
